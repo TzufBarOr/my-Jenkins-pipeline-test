e7ddbb7e786f2f4abfb1cd0e8c0b42a3afb16102
Tzuf <117042731+TzufBarOr@users.noreply.github.com>
2022-11-28 22:28:45 +0200

    Delete showing host name and IP.py

--- showing host name and IP.py
+++
@@ -1,6 +0,0 @@
-#this program telling what is Your host name and Your private ip address
-import socket
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-print("Your Computer Name is:" + hostname)
-print("Your Computer IP Address is:" + IPAddr)
